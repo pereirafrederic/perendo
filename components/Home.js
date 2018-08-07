1ec51dc85e9140a23fda3fb26ef4178f6a0f1b84
pereira frédéric <pereira.frederic.developpeur@gmail.com>
2018-08-07 18:11:58 +0200

    fix

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import {View, Text, StyleSheet}from 'react-native'
-import Menu from './Menu'
-
 
 export default class Home extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      title: 'menu',
+      menus: [
+
+  { "id": 1, "title":"définition des PE" },
+
+  { "id": 2, "title":"Sources" },
+
+  { "id": 3, "title":"Effets sur la santé" },
+
+  { "id": 4, "title":"Conseil de prévention" },
+
+  { "id": 5, "title":" Liens utiles" }
+
+  ]
+    }
+  }
   render() {
+      var _ = require('lodash');
     return (
 
       <View >
-         <Menu />
+      {this.state.title}
+        _.each(this.state.menus, (menu) => {<View> {menu.title}</View>})
       </View>
 
     );
