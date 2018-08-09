febfb8a99b7427aeb03cb9dd8e47a2ce3badebbe
Fred <pereira.frederic.developpeur@gmail.com>
2018-08-09 13:04:51 +0200

    init new project

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+  import { StyleSheet, Text, View, StatusBar } from 'react-native';
+  import { createStackNavigator } from 'react-navigation';
 
-import Home from './components/Home'
-import About from './components/About'
-
-
+  import Home from './components/Home'
+  import About from './components/About'
 
 
-export default class App extends React.Component {
-
-  const RootStack = createStackNavigator(
-    Home: {
-      screen: Home
-    },
-    {
-      initialRouteName: 'Home',
-    }
-  );
 
 
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <StatusBar hidden={true} />
-       <RootStack />;
-      </View>
+  export default class App extends React.Component {
+
+    const RootStack = createStackNavigator(
+      Home: {
+        screen: Home
+      },
+      {
+        initialRouteName: 'Home',
+      }
     );
+
+
+    render() {
+      return (
+        <View style={{flex: 1}}>
+          <StatusBar hidden={true} />
+         <RootStack />;
+        </View>
+      );
+    }
   }
-}
