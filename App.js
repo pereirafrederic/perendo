2ce38c343ab689ce0540838b47e440967b0a10ac
pereira frédéric <pereira.frederic.developpeur@gmail.com>
2018-08-09 11:01:21 +0200

    go

--- App.js
+++ App.js
@@ -6,16 +6,20 @@ import Home from './components/Home'
 import About from './components/About'
 
 
-const RootStack = createStackNavigator(
-  Home: {
-    screen: Home
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
+
 
 export default class App extends React.Component {
+
+  const RootStack = createStackNavigator(
+    Home: {
+      screen: Home
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
+
+
   render() {
     return (
       <View style={{flex: 1}}>
