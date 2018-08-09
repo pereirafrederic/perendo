ca7bdf9924f99ff299bc8f99e963a960da31b38f
Fred <pereira.frederic.developpeur@gmail.com>
2018-08-09 11:39:56 +0200

    init new project

--- App.js
+++ App.js
@@ -1,4 +1,34 @@
 import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
+});
+
+
+/*import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 
@@ -45,3 +75,4 @@ export default class App extends React.Component {
     );
   }
 }
+*/
