a98192fc8700cddcdf3247d0b9c2d54ee93503c4
Fred <pereira.frederic.developpeur@gmail.com>
2018-08-09 14:19:40 +0200

    init new project

--- App.js
+++ App.js
@@ -1,71 +1,56 @@
 import React from 'react';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'; // 2.11.2
 import { Button, Text, View, StyleSheet } from 'react-native';
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Details!</Text>
-      </View>
-    );
-  }
-}
+import { Ionicons } from '@expo/vector-icons'; // Version can be specified in package.json
+import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation'; // Version can be specified in package.json
 
 class HomeScreen extends React.Component {
-
-
-    constructor(props){
-    super(props)
-    this.state ={
-      title: 'menu',
-      menus: [
-          { "id": 1, "title":"définition des PE" },
-          { "id": 2, "title":"Sources" },
-          { "id": 3, "title":"Effets sur la santé" },
-          { "id": 4, "title":"Conseil de prévention" },
-          { "id": 5, "title":" Liens utiles" }
-         ]
-    }
-  }
-
-renderMenus(){
- var _ = require('lodash'); // 4.17.10
-  var content =  _.each(this.state.menus, function (id, title) {
-      return  <Text> {title}</Text>}
-    );
-console.log(content)
-    return content;
-}
-
   render() {
-
-
     return (
-              <View style={styles.container}>
+
+      <View style={styles.container}>
         <View style={styles.title}>
-          <Text>application perendo</Text>
+          <Text>applicationperendo</Text>
         </View>
-      <View style={styles.menus}>
+        <View style={styles.menus}>
 
-      {this.renderMenus()}
+          <Text>définition des perturbateurs endocrinien</Text>
+          <Text>Sources</Text>
+          <Text>Effets sur la santé</Text>
+          <Text>conseil de prévention</Text>
+          <Text>Liens utiles</Text>
+        </View>
       </View>
-              <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-
-      </View>
-         );
-
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22,
+   marginLeft: 10
+  },
+  title :{
+     fontSize: 30
+  },
+  menus: {
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    backgroundColor:"cyan"
+  }
+});
 
 class SettingsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {/* other code from before here */}
+        <Text>Settings!</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
@@ -75,89 +60,74 @@ class SettingsScreen extends React.Component {
   }
 }
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-  Details: DetailsScreen,
-});
+class DetailsScreen extends React.Component {
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-  Details: DetailsScreen,
-});
-
-const BottomTabNavigator = createBottomTabNavigator(
-  {
-    Home: HomeStack,
-    Settings: SettingsStack,
-  },
-  {
-      tabBarOptions: {
-     labelStyle: {
-       fontSize: 12,
-     },
-     tabStyle: {
-       width: 100,
-     },
-     showIcon : false,
-     showLabel : true,
-     indicatorStyle :{
-       height: 2,
-       backgroundColor: "red"
-     },
-     style: {
-       backgroundColor: "#00859D",
-       borderTopWidth: 1,
-       borderColor: "pink",
-       justifyContent: 'center',
-       alignItems: 'center'
-     },
-   }
+    constructor(props){
+    super(props)
+    this.state ={
+      title: 'DetailsScreen2'
+    }
   }
-);
 
-  const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22,
-   marginLeft: 10,
-   alignItems: 'center'
-  },
-  menus: {
-    paddingTop: 20,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2
-  }
-});
-
-export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <BottomTabNavigator />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>{this.state.title}</Text>
       </View>
     );
   }
-
-
-
-  /*
- tabBarPosition: 'bottom',
-   tabBarOptions :{
-     showIcon : false,
-     showLabel : true,
-     indicatorStyle :{
-       height: 2,
-       backgroundColor: "#00859D"
-     },
-     style: {
-       backgroundColor: "#00859D",
-       borderTopWidth: 1,
-       borderColor: "#3f101c"
-
-     }
-   }
-
-
- */
 }
+
+const HomeStack = StackNavigator({
+  Home: { screen: HomeScreen },
+  Details: { screen: DetailsScreen },
+});
+
+const SettingsStack = StackNavigator({
+  Settings: { screen: SettingsScreen },
+  Details: { screen: DetailsScreen },
+});
+
+export default TabNavigator(
+  {
+    Home: { screen: HomeStack },
+    Settings: { screen: SettingsStack },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: '#006400',
+      inactiveTintColor: 'black',
+      inactiveBackgroundColor : 'white',
+      indicatorStyle :{
+       height: 20,
+       backgroundColor: "red"
+     },
+     labelStyle: {
+    fontSize: 15,
+  },
+      style:{
+          backgroundColor: "#00859D",
+      }
+    },
+    animationEnabled: false,
+    swipeEnabled: false,
+
+  }
+);
