351173ffdf3a5d43485215737dadb23c22bf5bbc
Fred <pereira.frederic.developpeur@gmail.com>
2018-08-09 13:52:27 +0200

    init new project

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import {View, Text, StyleSheet}from 'react-native'
-import { createStackNavigator } from 'react-navigation';
+import React from 'react';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'; // 2.11.2
+import { Button, Text, View, StyleSheet } from 'react-native';
 
-export default class HomeScreen extends React.Component {
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Details!</Text>
+      </View>
+    );
+  }
+}
 
-  constructor(props){
+class HomeScreen extends React.Component {
+
+
+    constructor(props){
     super(props)
     this.state ={
       title: 'menu',
@@ -18,35 +29,98 @@ export default class HomeScreen extends React.Component {
     }
   }
 
+renderMenus(){
+ var _ = require('lodash'); // 4.17.10
+  var content =  _.each(this.state.menus, function (id, title) {
+      return  <Text> {title}</Text>}
+    );
+console.log(content)
+    return content;
+}
 
   render() {
-    return (
 
-      <View style={styles.container}>
+
+    return (
+              <View style={styles.container}>
         <View style={styles.title}>
-          <Text>applicationperendo</Text>
+          <Text>application perendo</Text>
         </View>
       <View style={styles.menus}>
 
-      <Text>définition des perturbateurs endocrinien</Text>
-      <Text>Sources</Text>
-      <Text>Effets sur la santé</Text>
-      <Text>conseil de prévention</Text>
-      <Text>Liens utiles</Text>
+      {this.renderMenus()}
       </View>
-      </View>
+              <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
 
+      </View>
+         );
+
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {/* other code from before here */}
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
     );
   }
 }
 
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+});
 
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+  Details: DetailsScreen,
+});
 
-const styles = StyleSheet.create({
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Settings: SettingsStack,
+  },
+  {
+      tabBarOptions: {
+     labelStyle: {
+       fontSize: 12,
+     },
+     tabStyle: {
+       width: 100,
+     },
+     showIcon : false,
+     showLabel : true,
+     indicatorStyle :{
+       height: 2,
+       backgroundColor: "red"
+     },
+     style: {
+       backgroundColor: "#00859D",
+       borderTopWidth: 1,
+       borderColor: "pink",
+       justifyContent: 'center',
+       alignItems: 'center'
+     },
+   }
+  }
+);
+
+  const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 22,
-   marginLeft: 10
+   marginLeft: 10,
+   alignItems: 'center'
   },
   menus: {
     paddingTop: 20,
@@ -55,3 +129,35 @@ const styles = StyleSheet.create({
     paddingBottom: 2
   }
 });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <BottomTabNavigator />
+      </View>
+    );
+  }
+
+
+
+  /*
+ tabBarPosition: 'bottom',
+   tabBarOptions :{
+     showIcon : false,
+     showLabel : true,
+     indicatorStyle :{
+       height: 2,
+       backgroundColor: "#00859D"
+     },
+     style: {
+       backgroundColor: "#00859D",
+       borderTopWidth: 1,
+       borderColor: "#3f101c"
+
+     }
+   }
+
+
+ */
+}
